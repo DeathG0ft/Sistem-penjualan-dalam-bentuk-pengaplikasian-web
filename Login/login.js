3218fb56a8116b99c63f81bea680dcc260f949fc
valentiosabatprasetiya-rgb <valentiosabatprasetiya.com>
2026-06-16 09:30:47 +0700

    penambahan file login

---
+++ Login/login.js
@@ -0,0 +1,9 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+
+    e.preventDefault();
+
+    alert("Login Berhasil!");
+
+    window.location.href = "index.html";
+
+});
